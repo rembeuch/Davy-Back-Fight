1f64361e524e4b6cd59783daa5054460f300cdba
Remi Beucherie <beucherie.r@gmail.com>
2019-12-25 12:33:12 +0100

    add berrys after one day connexion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_24_142735) do
+ActiveRecord::Schema.define(version: 2019_12_25_093954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,6 +65,11 @@ ActiveRecord::Schema.define(version: 2019_12_24_142735) do
     t.string "avatar"
     t.integer "berrys", default: 10000
     t.boolean "admin"
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet "current_sign_in_ip"
+    t.inet "last_sign_in_ip"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
