0df4f4997b9abdb1eb2536f4a5910b8d4d3de867
Remi Beucherie <beucherie.r@gmail.com>
2020-01-08 12:34:39 +0100

    fix bug user answer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_07_110017) do
+ActiveRecord::Schema.define(version: 2020_01_08_112101) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2020_01_07_110017) do
     t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "closed", default: false
     t.index ["answer_id"], name: "index_user_answers_on_answer_id"
     t.index ["user_id"], name: "index_user_answers_on_user_id"
   end
