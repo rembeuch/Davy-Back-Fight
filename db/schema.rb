176f38758029ee0561a91723cb8640daa68d36bf
Remi Beucherie <beucherie.r@gmail.com>
2020-01-09 18:01:41 +0100

    add new create product for admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_08_115829) do
+ActiveRecord::Schema.define(version: 2020_01_09_164359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,10 +53,11 @@ ActiveRecord::Schema.define(version: 2020_01_08_115829) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "photo_url"
+    t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
+    t.text "description"
   end
 
   create_table "questions", force: :cascade do |t|
