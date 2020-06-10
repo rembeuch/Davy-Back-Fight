fb95c2809f65fc4fd7daf9a5575971a9f49ce4c1
Remi Beucherie <beucherie.r@gmail.com>
2020-06-10 13:27:07 +0200

    nope bug mail

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,6 +1,5 @@
 class PaymentsController < ApplicationController
   def new
     @order = current_user.orders.where(state: 'Non finalisée').find(params[:order_id])
-           UserMailer.with(user: current_user).command.deliver_now
   end
 end
