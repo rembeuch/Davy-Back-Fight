d08b3e88106fe070bdcd14f25260563b71adf5a8
Remi Beucherie <beucherie.r@gmail.com>
2020-08-15 12:47:20 +0200

    add participations in show tournament

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -20,12 +20,27 @@ class TournamentsController < ApplicationController
   def show
     @tournament = Tournament.find(params[:id])
     @participation = Participation.new
-    @current_participation = current_user.participations.where(tournament: @tournament)
+    @current_participation = current_user.participations.where(tournament: @tournament).first
+    @participations_validées = Participation.all.where(tournament: @tournament, status: "Validée")
+    @participations_éliminées = Participation.all.where(tournament: @tournament, status: "éliminée")
+    odd_elements(@participations_validées)
   end
+
+  def odd_elements(array)
+    @participations_odd = []
+    @participations_even = []
+    array.each_with_index do |item,index|
+      if index % 2 == 0
+        @participations_odd.push(item)
+      else
+        @participations_even.push(item)
+      end
+    end
+  end
+
+  private
 
   def tournament_params
     params.require(:tournament).permit(:title, :start)
   end
 end
-
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_092743) do
+ActiveRecord::Schema.define(version: 2020_08_15_084506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -101,6 +101,7 @@ ActiveRecord::Schema.define(version: 2020_08_12_092743) do
     t.bigint "tournament_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "Validée"
     t.index ["tournament_id"], name: "index_participations_on_tournament_id"
     t.index ["user_id"], name: "index_participations_on_user_id"
   end
