0928ff2808feaca1cdd5c5c15c4141240608913d
Remi Beucherie <beucherie.r@gmail.com>
2020-11-19 10:33:58 +0100

    add coupon

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -49,7 +49,7 @@ class OrdersController < ApplicationController
         Stripe::Coupon.retrieve("#{order.coupon}")
         @coupon = Stripe::Coupon.retrieve("#{order.coupon}")
         order.amount -= (order.amount * (@coupon.percent_off / 100))
-      elsif order.coupon == "INSTA2000"
+      elsif order.coupon == "FOXY10"
         Stripe::Coupon.retrieve("#{order.coupon}")
         @coupon = Stripe::Coupon.retrieve("#{order.coupon}")
         order.amount -= (order.amount * (@coupon.percent_off / 100))
@@ -153,7 +153,7 @@ class OrdersController < ApplicationController
         @coupon = Stripe::Coupon.retrieve("#{order.coupon}")
         order.amount = @cart.total
         order.amount -= (order.amount * (@coupon.percent_off / 100))
-      elsif order.coupon == "INSTA2000"
+      elsif order.coupon == "FOXY10"
         Stripe::Coupon.retrieve("#{order.coupon}")
         @coupon = Stripe::Coupon.retrieve("#{order.coupon}")
         order.amount = @cart.total
